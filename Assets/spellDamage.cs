39d3d89879fc8434cc9eff34e1b4d1c0a2258572
pvincent <pvincent@student.42.fr>
2015-06-28 19:05:07 +0200

    spell collider in progress

---
+++ Assets/spellDamage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spellDamage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnParticleCollision(GameObject collid)
+	{
+		Debug.Log (gameObject.name);
+		if (collid.tag == "enemy")
+		{
+			stats e = collid.GetComponentInParent<enemy>().st;
+			if (e != null)
+			{
+				e.hp -= gameObject.GetComponentInParent<spellEffect>().damage;
+				if (e.hp <= 0)
+					e.hp = 0;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
