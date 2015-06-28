54abc730fc3872e4f8bd8d16b48921a7906485d2
pvincent <pvincent@student.42.fr>
2015-06-28 22:41:58 +0200

    Au pire hein..

---
+++ Assets/spellBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spellBar : MonoBehaviour {
+
+	public spellEffect		spell;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spell.canBeUse == false)
+			gameObject.GetComponent<CanvasRenderer>().SetColor(Color.grey);
+		else
+			gameObject.GetComponent<CanvasRenderer>().SetColor(Color.white);
+
+	}
+}
